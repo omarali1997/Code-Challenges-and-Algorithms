996550167051791e58d830eeb7ac2ebc2c222f09
omarali1997 <omar.aziz.9718@gmail.com>
2022-12-13 07:40:14 +0000

    fix it

--- python/code_challenges/hashtable/challenge01/challenge01.py
+++ python/code_challenges/hashtable/challenge01/challenge01.py
@@ -1,54 +1,58 @@
 # Write here the code challenge solution
 
 # create node class
-class TreeNode():
-    def __init__(self, value=0, left=None, right=None):
+class Node():
+    def __init__(self, value):
         self.value = value
-        self.left = left
-        self.right = right
-    # create function that sorted Array To BST
-    def sortedArrayToBST(self,num):
-        num.sort()
-        def BST_Tree(nums):
-            if not nums:
-                return None                                          
-            m = len(nums)//2
-            return (TreeNode(nums[m],BST_Tree(nums[:m]),BST_Tree(nums[m+1:])) )
-        root=BST_Tree(num)
-        return root
+        self.left = None
+        self.right = None
 
 
-# pre order function
-def pre_order(root , x):
-    if root is not None:
-        x.append(root.value)
-    if root.left is not None:
-        pre_order(root.left , x)
-    if root.right is not None:
-        pre_order(root.right , x)
-    return x
+class Tree:
+    def __init_(self):
+        self.root = None
 
-
-
-
-
-# function that takes a Binary Search tree and an integer as a parameter. Return True if Binary search tree has two elements that their summation is the given integer
-def summation(root,k):
-    x = pre_order(root,[])
-    for i in range(len(x)):
-        if (x[i] - k ) not in x:
-            continue
-        else:
-            return True
-    else:
+    # function that takes a Binary Search tree and an integer as a parameter. Return True if Binary search tree has two elements that their summation is the given integer
+    def summation(self, k):
+        if not self.root:
+            return False
+        x, y = [self.root], set()
+        for i in x:
+            if k - i.value in y:
+                return True
+            y.add(i.value)
+            if i.left:
+                x.append(i.left)
+            if i.right:
+                x.append(i.right)
         return False
 
-# to try the function it work 
 
+# to try the function it work
 if __name__ == "__main__":
 
-    tree = TreeNode()
-    T=tree.sortedArrayToBST([7,2,9,1,5,14])
-    print(summation(T,4))
-    
+    tree1 = Tree()
+    node1 = Node(7)
+    node2 = Node(2)
+    node3 = Node(9)
+    node4 = Node(1)
+    node5 = Node(5)
+    node6 = Node(14)
 
+    node1.left = node2
+    node1.right = node3
+    node2.left = node4
+    node2.right = node5
+    node3.right = node6
+
+    tree1.root = node1
+
+    # should return True
+    tree1.summation(3)
+    z = tree1.summation(3)
+    print(z)
+
+    # should return false
+    tree1.summation(4)
+    z = tree1.summation(4)
+    print(z)
--- python/code_challenges/hashtable/challenge01/test_challenge01.py
+++ python/code_challenges/hashtable/challenge01/test_challenge01.py
@@ -1,24 +1,33 @@
 # Write your test here
+from challenge01 import Tree, Node
 import pytest 
 
-from challenge01 import TreeNode ,summation
+#build the tree
+tree = Tree()
+node1 = Node(7)
+node2 = Node(2)
+node3 = Node(9)
+node4 = Node(1)
+node5 = Node(5)
+node6 = Node(14)
+node1.left = node2
+node1.right = node3
+node2.left = node4
+node2.right = node5
+node3.right = node6
+tree.root = node1
+
 # test k=3
 def test1():
-    k=3
-    tree1 = TreeNode()
-    T=tree1.sortedArrayToBST([7,2,9,1,5,14])
-    assert summation(T,k) == True
+    z = tree.summation(3)
+    assert z == True
 
 # test k=4
 def test2():
-    k=4
-    tree1 = TreeNode()
-    T=tree1.sortedArrayToBST([7,2,9,1,5,14])
-    assert summation(T,k) == True
+    z = tree.summation(4)
+    assert z == False
 
-# test k=22
+# test k=5
 def test3():
-    k=22
-    tree1 = TreeNode()
-    T=tree1.sortedArrayToBST([7,2,9,1,5,14])
-    assert summation(T,k) == False
+    z = tree.summation(5)
+    assert z == False
